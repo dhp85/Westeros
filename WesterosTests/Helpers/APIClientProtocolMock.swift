77bfd629ed183d8c95c9a3e92a7da67957c79a1a
Diego Herreros <diegohp85@gmail.com>
2024-09-26 13:21:41 +0200

    Feat: Creation Unit testing

--- WesterosTests/Helpers/APIClientProtocolMock.swift
+++ WesterosTests/Helpers/APIClientProtocolMock.swift
@@ -6,53 +6,28 @@
 //
 
 import Foundation
-// con @testable podemos acceder a todos las funcionalidades como si fueran public y no hay necesidad de ponerlas publicas a mano
 @testable import Westeros
 
-// el Mock es para inspeccionar propiedades y para enviar el resultado que queremos.
-// Necesitamos definir un generico con la misma restriccion que T para poder enviar valores al completion handler.
+// Necesitamos definir un generico con la misma restriccion que T
+// Para poder enviar valores al completion handler
 final class APIClientProtocolMock<C: Codable>: APIClientProtocol {
     var session: URLSession = .shared
     
-    //
     var didCallRequest = false
-    
-    var recivedRequest: URLRequest?
-    // Este generioc C tiene la misma restriccion "Codable" que T.
-    var recivedResult: Result<C, WesterosError>?
-    
-    func request<T>(_ type: T.Type, from request: URLRequest, completion: @escaping (Result<T, Westeros.WesterosError>) -> Void) where T : Decodable, T : Encodable {
-        
-        recivedRequest = request
+    var receivedRequest: URLRequest?
+    // Este generico C tiene la misma restriccion `Codable` que T
+    var receivedResult: Result<C, WesterosError>?
+    func request<T: Codable>(
+        _ type: T.Type,
+        from request: URLRequest,
+        completion: @escaping (Result<T, Westeros.WesterosError>) -> Void
+    ) {
+        receivedRequest = request
         didCallRequest = true
         
-        if let result = recivedResult as? Result<T,WesterosError> {
+        if let result = receivedResult as? Result<T, WesterosError> {
             completion(result)
         }
-        
     }
-    
-    
-    
 }
 
-// MARK: SPY
-//esto seria el concepto de spy, saber si estoy llamando al request
-
-/*final class APIClientProtocolMock: APIClientProtocol {
-    var session: URLSession = .shared
-    
-    // saber si llama a request
-    var didCallRequest = false
-    // testea la url que le llega
-    var recivedRequest: URLRequest?
-    
-    func request<T>(_ type: T.Type, from request: URLRequest, completion: @escaping (Result<T, Westeros.WesterosError>) -> Void) where T : Decodable, T : Encodable {
-        
-        recivedRequest = request
-        didCallRequest = true
-    }
-    
-    
-    
-}*/
--- WesterosTests/NetworkModelTest.swift
+++ WesterosTests/NetworkModelTest.swift
@@ -6,69 +6,60 @@
 //
 
 
-// CREACION DE UN UNIT TEST.
 import XCTest
 @testable import Westeros
 
 final class NetworkModelTests: XCTestCase {
-    //1º_ Definir mi unidad.
     private var sut: NetworkModel!
     private var mock: APIClientProtocolMock<[GOTCharacter]>!
-    // este metodo setUp() se va ha ejecutar cada vez que ejecutemos un metodo de XCTestCase
-    // SetUp actua como inicializador para el test unitario.
-    //Es un buen punto donde resetear el estado del test.
-    //Tambien inicializamos las propiedades.
+    
+    // SetUp actua como inicializador para el test unitario
+    // Es un buen punto donde resetear el estado del test
+    // Tambien inicializamos las propiedades
     override func setUp() {
         super.setUp()
-        
         mock = APIClientProtocolMock()
         sut = NetworkModel(client: mock)
     }
     
-
     // Estamos verificando el comportamiento del network model
     // cuando el APIClientProtocol devuelve un success
-
     func test_getAllCharacters_success() {
-        // Given, dado que tengo una condiciones.
+        // Given
         let someResult = Result<[GOTCharacter], WesterosError>.success([])
-        mock.recivedResult = someResult
-        var recivedResult: Result<[GOTCharacter], WesterosError>?
+        mock.receivedResult = someResult
+        var receivedResult: Result<[GOTCharacter], WesterosError>?
         let expectedURL = URL(string: "https://thronesapi.com/api/v2/Characters")!
         var expectedRequest = URLRequest(url: expectedURL)
         expectedRequest.httpMethod = "GET"
-        // When, cuando el objeto haga algo.
-        
+
+        // When
         sut.getAllCharacters { result in
-            recivedResult = result
+            receivedResult = result
         }
-            
-    
-        
-        // Then, verifico el estado.
-        
-        XCTAssertEqual(someResult, recivedResult)
+
+        // Then
+        XCTAssertEqual(someResult, receivedResult)
         XCTAssert(mock.didCallRequest)
-        XCTAssertEqual(mock.recivedRequest, expectedRequest)
+        XCTAssertEqual(
+            mock.receivedRequest,
+            expectedRequest
+        )
     }
     
     func test_getAllCharacters_failure() {
         // Given
         let someResult = Result<[GOTCharacter], WesterosError>.failure(.unknown)
-        mock.recivedResult = someResult
-        var recivedResult: Result<[GOTCharacter], WesterosError>?
-        
+        mock.receivedResult = someResult
+        var receivedResult: Result<[GOTCharacter], WesterosError>?
+
         // When
-        
         sut.getAllCharacters { result in
-            recivedResult = result
+            receivedResult = result
         }
-        
+
         // Then
-        
-        XCTAssertEqual(someResult, recivedResult)
-        
+        XCTAssertEqual(someResult, receivedResult)
+        XCTAssert(mock.didCallRequest)
     }
-    
-    
 }
